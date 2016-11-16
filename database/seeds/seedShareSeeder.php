f1447dc6f854d8c712dc97e763721f2ae9b960e0
vagrant <vagrant@homestead>
2016-11-16 20:39:42 +0000

    add seed files

---
+++ database/seeds/seedShareSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class seedShareSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+
+       for($i=1;$i<21;$i++){
+
+
+DB::table('shares')->insert([
+'name'=>str_random(10),
+'symobol'=>str_random(9),
+'count'=>rand(11,99)
+
+
+
+	]
+	);
+
+       }
+    }
+}
